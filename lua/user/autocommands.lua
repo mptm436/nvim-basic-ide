5f146077bc2d78c781a572dec287ae2adb7b766b
minimumax <liqiancn@gmail.com>
2022-08-16 00:10:04 +0800

    feat: add option manual fold

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,60 +1,60 @@
 -- Use 'q' to quit from common plugins
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
-  callback = function()
-    vim.cmd [[
+	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
+	callback = function()
+		vim.cmd([[
       nnoremap <silent> <buffer> q :close<CR> 
       set nobuflisted 
-    ]]
-  end,
+    ]])
+	end,
 })
 
 -- Remove statusline and tabline when in Alpha
 vim.api.nvim_create_autocmd({ "User" }, {
-  pattern = { "AlphaReady" },
-  callback = function()
-    vim.cmd [[
+	pattern = { "AlphaReady" },
+	callback = function()
+		vim.cmd([[
       set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
       set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
-    ]]
-  end,
+    ]])
+	end,
 })
 
 -- Set wrap and spell in markdown and gitcommit
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "gitcommit", "markdown" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
+	pattern = { "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
 })
 
-vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
+vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
 
 -- Fixes Autocomment
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-  callback = function()
-    vim.cmd "set formatoptions-=cro"
-  end,
+	callback = function()
+		vim.cmd("set formatoptions-=cro")
+	end,
 })
 
 -- Highlight Yanked Text
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  callback = function()
-    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
-  end,
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+	end,
 })
 
 -- returns to the previous edit position
 vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = { "*" },
-  callback = function ()
-    local test_line_data = vim.api.nvim_buf_get_mark(0, '\"')
-    local test_line = test_line_data[1]
-    local last_line = vim.api.nvim_buf_line_count(0)
+	pattern = { "*" },
+	callback = function()
+		local test_line_data = vim.api.nvim_buf_get_mark(0, '"')
+		local test_line = test_line_data[1]
+		local last_line = vim.api.nvim_buf_line_count(0)
 
-    if test_line > 0 and test_line <= last_line then
-      vim.api.nvim_win_set_cursor(0, test_line_data)
-    end
-  end
+		if test_line > 0 and test_line <= last_line then
+			vim.api.nvim_win_set_cursor(0, test_line_data)
+		end
+	end,
 })
